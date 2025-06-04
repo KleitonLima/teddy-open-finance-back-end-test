a01a94d4ab788c92d4cdd7f8dcd4f1fd27aa2a74
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-04 13:24:34 -0300

    feat(main): refactor main.ts to improve application setup and structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './resources/app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { swaggerConfig } from './config/swagger.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { setupDbConnection } from './database/config/connection.db';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.setGlobalPrefix('api/v0');
+  app.useGlobalPipes(new ValidationPipe());
+  swaggerConfig(app);
+  await setupDbConnection();
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
