790f19e04a615164428da01f5f08cce463226a9e
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-04 09:40:42 -0300

    feat(app): change method of controller and service to getAppStatus

--- src/resources/app/app.service.ts
+++ src/resources/app/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ENVCONFIG } from 'src/config/env.config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAppStatus(): string {
+    return `Server is running! 🚀\n Please check <a href="http://localhost:${ENVCONFIG.PORT}/api/v1/docs">http://localhost:${ENVCONFIG.PORT}/api/v1/docs</a> for Swagger docs...`;
   }
 }
