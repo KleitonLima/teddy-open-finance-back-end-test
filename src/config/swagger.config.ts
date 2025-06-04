62353e8ca9b6f1bb8974f9e6db061afa4dcd1936
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-04 09:39:48 -0300

    feat(swagger): added Swagger configuration to API documentation

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,17 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+
+export const swaggerConfig = (
+  app: NestExpressApplication<
+    Server<typeof IncomingMessage, typeof ServerResponse>
+  >,
+) => {
+  const docConfig = new DocumentBuilder()
+    .setTitle('API Documentation Teddy Open Finance Test')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('api/v1/docs', app, document);
+};
