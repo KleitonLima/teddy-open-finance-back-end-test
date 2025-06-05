df2fcaa87deffe76c74daa4b2f92718e74199460
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-05 16:24:03 -0300

    feat(migrations): migration to create shortened_url table

---
+++ src/database/migrations/1749151406644-crate-shortened-url-table.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CrateShortenedUrlTable1749151406644 implements MigrationInterface {
+  name = 'CrateShortenedUrlTable1749151406644';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "shortened_url" ("id" SERIAL NOT NULL, "original_url" character varying NOT NULL, "shortened_url" character varying NOT NULL, "accesses" integer NOT NULL DEFAULT '0', "deleted" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, CONSTRAINT "PK_786934fb957b601fcf913b24f04" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "shortened_url"`);
+  }
+}
