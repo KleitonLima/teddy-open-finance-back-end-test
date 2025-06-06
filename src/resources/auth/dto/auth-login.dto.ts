bbd6a04e04fe7c6b3cf3942b8366766748c550d2
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 20:27:20 -0300

    feat(auth): added email and password example to auth

--- src/resources/auth/dto/auth-login.dto.ts
+++ src/resources/auth/dto/auth-login.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class AuthLoginDto {
+  @ApiProperty({
+    description: 'Email do usuário para login',
+    example: 'user@example.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Senha do usuário para login',
+    example: 'password123',
+  })
   @IsNotEmpty()
   @MinLength(6)
   password: string;
