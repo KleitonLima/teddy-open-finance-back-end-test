6718ec0ee529797df6e41134c63e95083fcb97f5
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 20:22:32 -0300

    feat(users): define route for user creation in UsersController

--- src/resources/users/users.controller.ts
+++ src/resources/users/users.controller.ts
@@ -6,7 +6,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
+  @Post('create')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
