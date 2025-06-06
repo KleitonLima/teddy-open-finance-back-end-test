836fe5bb761f8235a3273b3d4ae049ed7c464e53
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 14:09:32 -0300

    feat(users): added description and example in CreateUserDto

--- src/resources/users/dto/create-user.dto.ts
+++ src/resources/users/dto/create-user.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Email para criar usuário',
+    example: 'user@example.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Senha para criar usuário',
+    example: 'password123',
+  })
   @IsNotEmpty()
   @MinLength(6)
   password: string;
