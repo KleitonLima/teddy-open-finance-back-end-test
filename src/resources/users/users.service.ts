e4020ac3864c3a9304a212ab56d272c9ed51f2f0
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 13:26:44 -0300

    feat(users): implement user creation with password hashing and repository integration

--- src/resources/users/users.service.ts
+++ src/resources/users/users.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import bcrypt from 'bcrypt';
+import { ENVCONFIG } from 'src/config/env.config';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+
+    const hashPassword = await bcrypt.hash(password, ENVCONFIG.PASS_SALT);
+
+    const user = this.usersRepository.create({
+      email,
+      password: hashPassword,
+    });
+
+    return this.usersRepository.save(user);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.usersRepository.findOne({ where: { id } });
   }
 }
