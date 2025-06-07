03bd9120d7227ace640f7771eaf13065e08ac31a
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-07 16:17:37 -0300

    feat(module): add ShortenedUrl entity to TypeOrmModule imports for database integration

--- src/resources/app/app.module.ts
+++ src/resources/app/app.module.ts
@@ -6,12 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from 'src/database/config/typeorm.db';
 import { AuthModule } from '../auth/auth.module';
 import { ShortenedUrlModule } from '../shortened-url/shortened-url.module';
+import { ShortenedUrl } from '../shortened-url/entities/shortened-url.entity';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
     ShortenedUrlModule,
+    TypeOrmModule.forFeature([ShortenedUrl]),
     TypeOrmModule.forRoot(AppDataSource.options),
   ],
   controllers: [AppController],
