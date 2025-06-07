03c1d6057d9e22079c29608c86347fd71caeee08
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 21:44:59 -0300

    fix(env): update default PORT and add BASE_URL to ENVCONFIG

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,9 +3,10 @@ import 'dotenv/config';
 const env = process.env;
 
 export const ENVCONFIG = {
-  PORT: env.PORT ?? 3000,
+  PORT: env.PORT ?? 3232,
   DATABASE_URL: env.DATABASE_URL,
   PASS_SALT: env.PASS_SALT ?? 10,
   JWT_SECRET: env.JWT_SECRET ?? 'jwt-secret',
   JWT_EXPIRATION: env.JWT_EXPIRATION ?? '10m',
+  BASE_URL: env.BASE_URL ?? 'http://localhost:3232',
 };
