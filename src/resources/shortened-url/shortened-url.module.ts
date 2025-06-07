93be70fbbb61e69a3c4dd221e16f3bff33b2b54d
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 23:32:05 -0300

    feat(module): add TypeOrmModule and RandomCodeUtil to ShortenedUrlModule

--- src/resources/shortened-url/shortened-url.module.ts
+++ src/resources/shortened-url/shortened-url.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ShortenedUrlService } from './shortened-url.service';
 import { ShortenedUrlController } from './shortened-url.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShortenedUrl } from './entities/shortened-url.entity';
+import { RandomCodeUtil } from '../../utils/random-code.util';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ShortenedUrl])],
   controllers: [ShortenedUrlController],
-  providers: [ShortenedUrlService],
+  providers: [ShortenedUrlService, RandomCodeUtil],
 })
 export class ShortenedUrlModule {}
