627ffe274cd0aadbd1e35e60cf440a01113903b4
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 23:31:37 -0300

    fix(dto): remove optional user ID field from CreateShortenedUrlDto

--- src/resources/shortened-url/dto/create-shortened-url.dto.ts
+++ src/resources/shortened-url/dto/create-shortened-url.dto.ts
@@ -1,16 +1,9 @@
-import { IsString, IsUrl, IsOptional } from 'class-validator';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateShortenedUrlDto {
   @ApiProperty({ example: 'https://www.exemplo.com/exemplo' })
   @IsString()
   @IsUrl()
   originalUrl: string;
-
-  @ApiPropertyOptional({
-    example: '4d1db414-be1a-4237-a0ea-d888d0f81a5b',
-    description: 'ID do usuário dono da URL',
-  })
-  @IsOptional()
-  user?: string;
 }
