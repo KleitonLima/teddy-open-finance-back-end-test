d847514ac9056c665f897d95a5057018b823814b
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-07 14:27:01 -0300

    feat(migration): alter id column type to uuid in shortened_url table

--- src/database/migrations/1749151406644-crate-shortened-url-table.ts
+++ src/database/migrations/1749151406644-crate-shortened-url-table.ts
@@ -5,7 +5,7 @@ export class CrateShortenedUrlTable1749151406644 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "shortened_url" ("id" SERIAL NOT NULL, "original_url" character varying NOT NULL, "shortened_url" character varying NOT NULL, "accesses" integer NOT NULL DEFAULT '0', "deleted" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, CONSTRAINT "PK_786934fb957b601fcf913b24f04" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "shortened_url" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "original_url" character varying NOT NULL, "shortened_url" character varying NOT NULL, "accesses" integer NOT NULL DEFAULT '0', "deleted" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, CONSTRAINT "PK_786934fb957b601fcf913b24f04" PRIMARY KEY ("id"))`,
     );
   }
 
