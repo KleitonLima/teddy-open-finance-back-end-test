d929fd10229e7b296c3c5319b07d4d49df93f957
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 23:31:15 -0300

    feat(auth): add OptionalAuthGuard for handling optional JWT authentication

---
+++ src/resources/auth/optional-auth.guard.ts
@@ -0,0 +1,40 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { ENVCONFIG } from 'src/config/env.config';
+
+@Injectable()
+export class OptionalAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      return true;
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: ENVCONFIG.JWT_SECRET,
+      });
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+
+    return type === 'Bearer' ? token : undefined;
+  }
+}
