06cc26572da6cac7e149b824dfa1ff8bc3acdc35
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 23:32:15 -0300

    feat(service): implement create method for shortened URLs with URL validation and user association

--- src/resources/shortened-url/shortened-url.service.ts
+++ src/resources/shortened-url/shortened-url.service.ts
@@ -1,11 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateShortenedUrlDto } from './dto/create-shortened-url.dto';
 import { UpdateShortenedUrlDto } from './dto/update-shortened-url.dto';
+import { isURL } from 'class-validator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ShortenedUrl } from './entities/shortened-url.entity';
+import { Repository } from 'typeorm';
+import { ENVCONFIG } from 'src/config/env.config';
+import { RandomCodeUtil } from 'src/utils/random-code.util';
+import { Request } from 'express';
 
 @Injectable()
 export class ShortenedUrlService {
-  create(createShortenedUrlDto: CreateShortenedUrlDto) {
-    return 'This action adds a new shortenedUrl';
+  constructor(
+    @InjectRepository(ShortenedUrl)
+    private readonly shortenedUrlRepository: Repository<ShortenedUrl>,
+    private readonly randomCodeUtils: RandomCodeUtil,
+  ) {}
+
+  create(createShortenedUrlDto: CreateShortenedUrlDto, req: Request) {
+    const { originalUrl } = createShortenedUrlDto;
+
+    if (!isURL(originalUrl)) {
+      throw new BadRequestException('URL inválida');
+    }
+
+    const randomCode = this.randomCodeUtils.generate();
+    const shortUrl = ENVCONFIG.BASE_URL + '/' + randomCode;
+
+    const user = req.user;
+
+    const shortenedUrl = this.shortenedUrlRepository.create({
+      original_url: originalUrl,
+      short_url: shortUrl,
+      ...(user && { user: { id: user.id } }),
+    });
+
+    return this.shortenedUrlRepository.save(shortenedUrl);
   }
 
   findAll() {
