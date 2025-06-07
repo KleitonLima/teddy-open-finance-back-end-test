679b66840c2c097fb793bd5a1790d64f19de7aef
Kleiton Lima <kleiton.mini@gmail.com>
2025-06-06 23:28:10 -0300

    fix(module): add ShortenedUrlModule to AppModule imports

--- src/resources/app/app.module.ts
+++ src/resources/app/app.module.ts
@@ -5,11 +5,13 @@ import { UsersModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from 'src/database/config/typeorm.db';
 import { AuthModule } from '../auth/auth.module';
+import { ShortenedUrlModule } from '../shortened-url/shortened-url.module';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
+    ShortenedUrlModule,
     TypeOrmModule.forRoot(AppDataSource.options),
   ],
   controllers: [AppController],
